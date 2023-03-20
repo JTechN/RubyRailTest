0f17b0def8b9b97617f2e5fa9a1efd02c14aa435
JTechN <97194901+JTechN@users.noreply.github.com>
2023-03-20 13:07:52 -0400

    Prevent duplicate account numbers and names

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,13 +1,11 @@
 class Account < ApplicationRecord
-  # devise :database_authenticatable, :registerable,
-  #        :recoverable, :rememberable, :validatable
+
 
     # 0 = user, 1 = manager, 2 = admin
   enum :category, asset: "Asset", liability: "Liability", equity: "Equity"
   # enum :subcategory, asset: "Asset", liability: "Liability", equity: "Equity"
   enum :statement, is: "Income Statement", bs: "Balance Sheet" , re: "Retained Earning Statement"
-  # after_initialize :set_default_role, :if => :new_record?
-  # def set_default_role
-  #   self.category ||= :
-  # end
+
+  validates_uniqueness_of :number, :name
+
 end
