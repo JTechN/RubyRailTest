6537b9c97ea843896125e6a94700cb81ba96a523
jblake33 <jcb1354@gmail.com>
2023-03-20 09:25:02 -0400

    added images to resources

--- db/schema.rb
+++ db/schema.rb
@@ -39,7 +39,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_16_235606) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.string "surname"
-    t.integer "role"
+    t.integer "role", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
